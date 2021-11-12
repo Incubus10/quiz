538d0d73cac6a3d7db59407058349c7010b93010
Incubus10 <78927240+Incubus10@users.noreply.github.com>
2021-11-13 02:52:39 +0530

    Create quiz.c

---
+++ quiz.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<windows.h>
+
+int main(){
+    char c;
+    int d;
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("                                                 Welcome to My Quiz\n");
+    printf("                                                 ******************\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("The question that are from General Knowledge and all question have 4 option and just type the option either 'a','b','c','d'.\n\n");
+    printf("If you are ready to start the quiz just type start and type the number of question from 1 to 10\n");
+    scanf("%c %d",&c,&d);
+
+    for(int i=0;i<d;i++){
+        
+    }
+    
+
+}
