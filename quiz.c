f20fd14ae14dd743234b158a55cb2a01654f8642
Incubus10 <78927240+Incubus10@users.noreply.github.com>
2021-11-13 14:40:45 +0530

    Update quiz.c

--- quiz.c
+++ quiz.c
@@ -2,24 +2,104 @@
 #include<stdlib.h>
 
 
+voi dquestion(int d){
+    
+    switch(d){
+        case 1:printf("1.Brass gets discoloured in air because of the presence of which of the following gases in air ?\n"
+
+"A.	Oxygen\n"
+"B.	Hydrogen sulphide\n"
+"C.	Carbon dioxide\n"
+"D.	Nitrogen\n");
+break;
+        case 2: printf("\n2.Which of the following is a non metal that remains liquid at room temperature ?\n"
+
+"A.	Phosphorous\n"
+"B.	Bromine\n"
+"C.	Chlorine\n"
+"D.	Helium\n");
+break;
+
+        case 3:printf("3.Chlorophyll is a naturally occurring chelate compound in which central metal is ?\n"
+
+"A.copper\n"
+"B.	magnesium\n"
+"C.	iron\n"
+"D.	calcium\n");
+break;
+
+case 4: printf("4.Which of the following is used in pencils?\n"
+
+"A.	Graphite\n"
+"B.	Silicon\n"
+"C.	Charcoal\n"
+"D.	Phosphorous\n");
+break;
+case 5: printf("5.Which of the following metals forms an amalgam with other metals ?\n"
+
+"A.	Tin\n"
+"B.	Mercury\n"
+"C.	Leadv"
+"D.	Zinc\n");
+break;
+case 6: printf("6.Chemical formula for water is ?v"
+
+"A.	NaAlO2\n"
+"B.	H2O\n"
+"C.	Al2O3\n"
+"D.	CaSiO3\n");
+break;
+
+case 7: printf("7.The gas usually filled in the electric bulb is ?\n"
+
+"A.	nitrogen\n"
+"B.	hydrogen\n"
+"C.	carbon dioxide\n"
+"D.	oxygen\n");
+break;
+
+case 8:printf("8.Washing soda is the common name for ?\n"
+
+"A.	Sodium carbonate\n"
+"B.	Calcium bicarbonate\n"
+"C.	Sodium bicarbonate\n"
+"D.	Calcium carbonate\n");
+break;
+
+case 9:printf("9.Quartz crystals normally used in quartz clocks etc. is chemically ?\n"
+
+"A.	silicon dioxide\n"
+"B.	germanium oxidev"
+"C.	a mixture of germanium oxide and silicon dioxide\n"
+"D.	sodium silicate\n");
+break;
+case 10:printf("10.Which of the gas is not known as green house gas ?\n"
+
+"A.	Methane\n"
+"B.	Nitrous oxide\n"
+"C.	Carbon dioxide\n"
+"D.	Hydrogen\n");
+break;
+default:printf("The question has ended\n");
+break;
+    }
+}
 int main(){
-    char c;
-    int d;
+    char c,ans,ans2;
+    int d=0;
+    int a=0;
     printf("\n");
     printf("\n");
     printf("\n");
-    printf("                                                 Welcome to My Quiz\n");
-    printf("                                                 ******************\n");
+    printf("|-----------------------------------------------------------------------------------------------------|\n");
+    printf("|                                                 Welcome to My Quiz                                  |\n");
+    printf("|                                                 ******************                                  |\n");
+    printf("|-----------------------------------------------------------------------------------------------------|\n");
     printf("\n");
     printf("\n");
     printf("\n");
     printf("The question that are from General Knowledge and all question have 4 option and just type the option either 'a','b','c','d'.\n\n");
     printf("If you are ready to start the quiz just type start and type the number of question from 1 to 10\n");
-    scanf("%c %d",&c,&d);
-
-    for(int i=0;i<d;i++){
-        
-    }
     
 
 }
